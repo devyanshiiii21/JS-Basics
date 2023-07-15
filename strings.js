bb87dc94741dfc476ec200938efd21657579d362
Devyanshi Srivastava <devyanshi2106@gmail.com>
2023-07-15 14:56:30 +0530

    HTML & CSS

---
+++ strings.js
@@ -0,0 +1,9 @@
+// Template strings
+// helpful in interpolation-> helps in inserting the value directly to string
+
+console.log(`Items (${1+1}): $${(2899+1899)/100}`)
+
+//Multiline string
+
+`hey!
+how you doin`

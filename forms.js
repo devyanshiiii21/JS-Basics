ac9abf885ad2c4181bff22141788fdc9003a5c15
Devyanshi Srivastava <devyanshi2106@gmail.com>
2024-01-03 19:26:47 +0530

    Keyboard events

--- forms.js
+++ forms.js
@@ -1,12 +1,12 @@
 const form = document.querySelector(".signup");
 const feedback = document.querySelector(".feedback");
+const pattern = /^[a-zA-Z]{6,12}$/;
 
 form.addEventListener('submit', e => {
     e.preventDefault() // To prevent the refresh of page
 
     // Form validation
     const username = form.username.value;
-    const pattern = /^[a-zA-Z]{6,12}$/;
 
     if(pattern.test(username)){
         feedback.textContent = "Username is valid";
@@ -20,10 +20,25 @@ form.addEventListener('submit', e => {
 //Regular Expression
 
 const username = "raliya";
-const pattern = /[a-z]{6,}/;
 
 let result = pattern.test(username);
 console.log(result);
 
 let result2 = username.search(pattern);
 console.log(result2);
+
+
+// Keyboard events
+// Live feedback
+
+form.username.addEventListener('keyup', e =>{
+    // console.log(e.target.value, form.username.value);
+    if(pattern.test(form.username.value)){
+        console.log('passed');
+        form.username.setAttribute('class', 'success');
+    }
+    else{
+        console.log('failed');
+        form.username.setAttribute('class', 'failure');
+    }
+});
